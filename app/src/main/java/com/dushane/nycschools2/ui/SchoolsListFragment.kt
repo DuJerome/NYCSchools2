c83ee693a7d9bd8e86b7c6763bfd731ed2f6b8e5
Dushane <dushanesmith@gmail.com>
2023-04-02 07:42:22 -0400

    now fixing display of data but the cannot find view that should be visibile

--- app/src/main/java/com/dushane/nycschools2/ui/SchoolsListFragment.kt
+++ app/src/main/java/com/dushane/nycschools2/ui/SchoolsListFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.dushane.nycschools2.R
+import com.dushane.nycschools2.model.School
 import com.dushane.nycschools2.ui.viewmodel.SchoolViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -15,21 +18,16 @@ import io.reactivex.rxjava3.schedulers.Schedulers
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 @AndroidEntryPoint
-class SchoolsListFragment(): Fragment(R.layout.fragment_school_list) {
+class SchoolsListFragment: Fragment(R.layout.fragment_school_list) {
 
     private val schoolViewModel: SchoolViewModel by viewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val data = schoolViewModel.schools.observeOn(Schedulers.io()).blockingGet()
-        val adapter = SchoolListRecyclerViewAdapter(context, data)
-        adapter.notifyDataSetChanged()
-        return inflater.inflate(R.layout.fragment_school_list, container, false)
-    }
+    private lateinit var data: MutableList<School>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        data = schoolViewModel.schools.subscribeOn(Schedulers.io()).blockingGet()
+        val adapter = SchoolListRecyclerViewAdapter(context, data)
+        val recyclerView: RecyclerView = view.findViewById<RecyclerView>(R.id.recyclerViewSchoolList)
+        adapter.notifyDataSetChanged()
     }
 }
