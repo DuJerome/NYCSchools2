dd0d1f40963d37e1e240f7a45f3805512ce139e8
Dushane <dushanesmith@gmail.com>
2023-04-02 21:37:36 -0400

    Updated UI

--- app/src/main/java/com/dushane/nycschools2/ui/SATScoresListFragment.kt
+++ app/src/main/java/com/dushane/nycschools2/ui/SATScoresListFragment.kt
@@ -2,9 +2,7 @@ package com.dushane.nycschools2.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.paging.PagingData
@@ -23,14 +21,6 @@ import kotlinx.android.synthetic.main.fragment_sat_scores_list.*
 class SATScoresListFragment : Fragment() {
 
     private val satViewModel:SATScoresViewModel by viewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        return view
-    }
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
